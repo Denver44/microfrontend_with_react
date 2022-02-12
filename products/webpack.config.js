a807b8a1ad87c0fc5c937447b261d1ea224be729
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-12 11:28:01 +0530

    A Touch More on Webpack done

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,3 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// This plugin actually helps us to embedded the js file with the HTML File automatically
+
 module.exports = {
   mode: 'development',
+  devServer: {
+    port: 8081, // We have created a dev Server on 8081 port
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // We need to give the location of our HTML FIle in which we will embedded the js file which is process or build by webpack
+    }),
+  ],
 };
