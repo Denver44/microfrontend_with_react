bd4fa7947ec318daf93dd87ef56390060cf11fb8
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-12 11:32:15 +0530

    Finishing the Product List done

--- products/src/index.js
+++ products/src/index.js
@@ -2,9 +2,11 @@ import faker from 'faker';
 
 let products = '';
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 10; i++) {
   let name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
 
 console.log(products);
+
+document.getElementById('dev-products').innerHTML = products;
